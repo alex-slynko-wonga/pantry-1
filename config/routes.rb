da82c1c83dabcf7bb34fe065a8d293c095cc454d
Justin Connell <justinconnell@users.noreply.github.com>
2013-06-05 16:25:34 +0100

    TD-770 Package controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Pantry::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  resources :packages, only: :create
-
+  resources :packages, only: [:create, :index]
   root to: 'home#index'
 end
