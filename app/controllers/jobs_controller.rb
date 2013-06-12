50e70e5d7c724caaba693d7260fa4291c31910d4
John Coleman <john-coleman@users.noreply.github.com>
2013-06-12 09:58:13 +0100

    TD-852 TD-853 Adding create method and index method and view to JobController

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,10 @@
 class JobsController < ApplicationController
+  def create
+    Job.create(params.permit(:name, :description))
+    render nothing: true
+  end
+
+  def index
+    @jobs = Job.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pantry::Application.routes.draw do
   resources :data_bags, only: [:index]
-  resources :job_logs, only: [:create, :index]
+  resources :job_logs, only: [:index]
+  resources :jobs, only: [:create, :index]
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   resources :packages, only: [:create, :index]
