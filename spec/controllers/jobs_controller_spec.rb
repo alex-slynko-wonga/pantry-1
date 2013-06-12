050054f5bec311fc5c1b5c5e2b5f0506c8eb5693
Alex Slynko <slynko@outlook.com>
2013-06-12 16:28:07 +0100

    fixed tests

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe JobsController do
   describe 'POST #create' do
     it "creates a job" do
-      time_now = Time.local(2000, 1, 1, 1, 0, 0)
-      Timecop.freeze(time_now)
       expect{ post :create, {
         name: 'Name', description: 'Description', status: 'Status' }
       }.to change(Job, :count).by(1)
@@ -13,7 +11,6 @@ describe JobsController do
       job.name.should == 'Name'
       job.description.should == 'Description'
       job.status.should == 'Status'
-      job.start_time.should == time_now
     end
   end
 end
--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -4,7 +4,7 @@ describe Package do
   subject { FactoryGirl.create(Package) }
   context "#update_data_bag" do
     it "updates data bag using DataBagResource" do
-      DataBagResource.should_receive(:create_or_updatei_item)
+      DataBagResource.should_receive(:create_or_update_item)
       subject.update_data_bag
     end
 
