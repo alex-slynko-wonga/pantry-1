717e46b816ee7ef082e451b453a70da0f4a865d9
John Coleman <john-coleman@users.noreply.github.com>
2013-06-17 16:38:02 +0100

    TD-752 TD-856 Improving job_log cucumber steps and combining the scenarios in to one

--- features/job_log.feature
+++ features/job_log.feature
@@ -2,17 +2,14 @@ Feature: Tracking external agent job logs
 
     Pantry needs to track job logs and keep a record of each job log history.
 
-    Scenario: An agent creates a new job log for a job
+    Scenario: An agent creates a new job log for a job and updates the log text
         Given an agent has registered a job on Pantry
         When an agent creates a job log for the job with LogText "Test Message"
-        And I am on the job logs page for the job
+        And I am on the job log page for the job
         Then I should see "Test Message"
-
-    Scenario: An agent updates a job log with a message
-        Given an agent has registered a new job on Pantry
-        And an agent has registered a job log for the job with LogText "Initial Message"
+        Then I should see the job id
         When I update the job log for the job with LogText "Appended Message"
         And I am on the job log page for the job
         Then I should see the job id
-        And I should see "Initial Message"
+        And I should see "Test Message"
         And I should see "Appended Message"
--- features/step_definitions/job_log_steps.rb
+++ features/step_definitions/job_log_steps.rb
@@ -1,7 +1,8 @@
 When(/^an agent creates a job log for the job with LogText "(.*?)"$/) do |message|
-  @job_log_response = page.driver.post "/jobs/#{@job.id}/job_logs", {job_id: @job.id, log_text: message}
-  @job_log_response.status.should == 201
-  @job_log_response.body.should include("#{host}/jobs/#{@job.id}/job_logs/")
+  @job_log_create_response = page.driver.post "/jobs/#{@job.id}/job_logs", {job_id: @job.id, log_text: message}
+  @job_log_create_response.status.should == 201
+  @job_log ||= JobLog.last
+  @job_log_create_response.body.should include("#{host}/jobs/#{@job_log.job_id}/job_logs/#{@job_log.id}")
 end
 
 When(/^I am on the job logs page for the job$/) do
@@ -9,11 +10,8 @@ When(/^I am on the job logs page for the job$/) do
 end
 
 When(/^I am on the job log page for the job$/) do
+  @job_log ||= JobLog.last
   visit "/jobs/#{@job_log.job_id}/job_logs/#{@job_log.id}"
-end
-
-Given(/^an agent has registered a new job on Pantry$/) do
-  @job = FactoryGirl.create(:job)
 end
 
 Given(/^an agent has registered a job log for the job with LogText "(.*?)"/) do |message|
@@ -21,7 +19,9 @@ Given(/^an agent has registered a job log for the job with LogText "(.*?)"/) do 
 end
 
 When(/^I update the job log for the job with LogText "(.*?)"$/) do |update_message|
-  @job_log_update = page.driver.put "/jobs/#{@job_log.job_id}/job_logs/#{@job_log.id}", {log_text: update_message}
+  @job_log ||= JobLog.last
+  @job_log_update_response = page.driver.put "/jobs/#{@job_log.job_id}/job_logs/#{@job_log.id}", {log_text: update_message}
+  @job_log_update_response.status.should == 200
 end
 
 Then(/^I should see the job id$/) do
