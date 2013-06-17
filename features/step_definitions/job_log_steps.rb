6873bd73ee4c54fbe5116777e41a270fa305f7ca
John Coleman <john-coleman@users.noreply.github.com>
2013-06-17 17:13:50 +0100

    TD-752 TD-856 removing unecessary job_log steps

--- features/step_definitions/job_log_steps.rb
+++ features/step_definitions/job_log_steps.rb
@@ -6,16 +6,10 @@ When(/^an agent creates a job log for the job with LogText "(.*?)"$/) do |messag
 end
 
 When(/^I am on the job log page for the job$/) do
-  @job_log ||= JobLog.last
   visit "/jobs/#{@job_log.job_id}/job_logs/#{@job_log.id}"
 end
 
-Given(/^an agent has registered a job log for the job with LogText "(.*?)"/) do |message|
-  @job_log = FactoryGirl.create(:job_log, job: @job, log_text: message)
-end
-
 When(/^I update the job log for the job with LogText "(.*?)"$/) do |update_message|
-  @job_log ||= JobLog.last
   @job_log_update_response = page.driver.put "/jobs/#{@job_log.job_id}/job_logs/#{@job_log.id}", {log_text: update_message}
   @job_log_update_response.status.should == 200
 end
