244ccdc81298dfbb2dfb8e8e7c3ba394737b772a
Brendan Rooney <brendan.rooney@wonga.com>
2013-06-19 16:12:28 +0100

    TD-775 TD-878 Add method to return single node from Chef

--- lib/chef_node_resource.rb
+++ lib/chef_node_resource.rb
@@ -3,4 +3,10 @@ module ChefNodeResource
     query = Chef::Search::Query.new
     nodes = query.search('node', "chef_environment:#{environment} AND role:#{role}")
   end
+  def self.findByName(name)
+    query = Chef::Search::Query.new
+    node = query.search('node',"name:#{name}").first
+  end
+  
+  
 end
