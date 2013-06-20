1a6f576ebfc29b4027e8c3bb4774061f12afdc01
Alex Slynko <slynko@outlook.com>
2013-06-20 10:14:32 +0100

    win_rm runner

---
+++ lib/win_rm_runner.rb
@@ -0,0 +1,20 @@
+class WinRMRunner
+  def add_host(host, user="Administrator", password="LocalAdminPassword")
+    session.use("#{user}@#{host}", {keys: key, keys_only: true })
+  end
+
+  def run_commands(*commands, &block)
+    commands.each do |command|
+      session.exec command do |ch, stream, data|
+        block.call(ch[:host], data) if block
+      end
+    end
+    session.loop
+    session.close
+  end
+
+  private
+  def session
+    @session ||= Net::SSH::Multi.start
+  end
+end
