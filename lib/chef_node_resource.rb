a26eabacc428f21317a23eac16347226802da1fe
Alex Slynko <slynko@outlook.com>
2013-06-20 13:18:17 +0100

    NO STORY: fix merge

--- lib/chef_node_resource.rb
+++ lib/chef_node_resource.rb
@@ -1,12 +1,6 @@
 module ChefNodeResource
-<<<<<<< HEAD
-  def self.listNodes(environment, role)
-    query = Chef::Search::Query.new
-    nodes = query.search('node', "chef_environment:#{environment} AND role:#{role}")
-=======
   def self.list_nodes(environment, role)
     query = Chef::Search::Query.new
     nodes = query.search('node', "chef_environment:#{environment} AND role:#{role}")[0]
->>>>>>> contributer/TD-819_Node_Search_Resource
   end
 end
