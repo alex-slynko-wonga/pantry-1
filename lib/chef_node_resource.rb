8b52452f749005345d76a34c52a2385b9d5803a8
Brendan Rooney <brendan.rooney@wonga.com>
2013-06-20 14:08:58 +0100

    TD-878 rename method get_single_node

--- lib/chef_node_resource.rb
+++ lib/chef_node_resource.rb
@@ -4,7 +4,7 @@ module ChefNodeResource
     nodes = query.search('node', "chef_environment:#{environment} AND role:#{role}")
   end
   
-  def self.findByName(name)
+  def self.find_by_name(name)
     query = Chef::Search::Query.new
     node = query.search('node',"name:#{name}").first
   end
