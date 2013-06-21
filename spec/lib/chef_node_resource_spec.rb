0a32d4df5d7e320a469ec2b66175de9803ccf1ae
Brendan Rooney <brendan.rooney@wonga.com>
2013-06-21 13:25:51 +0100

    Refactoring RSPEC

--- spec/lib/chef_node_resource_spec.rb
+++ spec/lib/chef_node_resource_spec.rb
@@ -1,24 +1,35 @@
 require 'spec_helper'
 
 describe ChefNodeResource do
-  let(:node_name) { 'awinpaintwwwl01.test.example.com' }
-  let(:environment) {'awinpaint' }
-  let(:role) {'*' }
+  let(:node_name) { 'test' }
+  let(:environment) {'test' }
+  let(:role) {'test' }
   describe "#get_list_of_nodes" do
     context "if node matches the environment and node params" do
       it "should return each node that matched the query" do
-        nodes = [Chef::Node.stub(:name).with(node_name)]
-        ChefNodeResource.stub(:listNodes).with(environment, role).and_return(nodes)    
-        expect(nodes.length).to be_eql 1 
+        search = mock
+        Chef::Search::Query.stub(:new).and_return(search)
+        search.stub(:search).with("node", "chef_environment:env AND role:role").and_return([['node']])
+        
+        nodes = ChefNodeResource.list_nodes('env', 'role')
+        
+        expect(nodes.length).to be_eql 1
+        expect(nodes[0]).to be_eql 'node'
       end
     end
   end
+
   describe "#get_single_node" do
     context "if node matches the name param" do
       it "should return a single node that matchs the query" do
-        nodes = [Chef::Node.stub(:name).with(node_name)]
-        ChefNodeResource.stub(:findByName).with(node_name).and_return(nodes)
+        search = mock
+        Chef::Search::Query.stub(:new).and_return(search)
+        search.stub(:search).with("node", "name:name").and_return([['node']])
+        
+        nodes = ChefNodeResource.get_single_node('env', 'name')
+        
         expect(nodes.length).to be_eql 1
+        expect(nodes[0]).to be_eql 'node'
       end
     end    
   end
