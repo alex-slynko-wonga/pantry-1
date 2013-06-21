ed5b8dab809ac4f5d6df7443fd72929f2c4190b1
Alex Slynko <slynko@outlook.com>
2013-06-21 13:49:52 +0100

    cleaned-up tests

--- spec/lib/chef_node_resource_spec.rb
+++ spec/lib/chef_node_resource_spec.rb
@@ -4,25 +4,26 @@ describe ChefNodeResource do
   let(:node_name) { 'test' }
   let(:environment) {'test' }
   let(:role) {'test' }
-  describe "#get_list_of_nodes" do
+
+  describe "#list_nodes" do
     context "if node matches the environment and node params" do
       it "should return each node that matched the query" do
-        search = mock
+        search = double
         Chef::Search::Query.stub(:new).and_return(search)
         search.stub(:search).with("node", "chef_environment:env AND role:role").and_return([['node']])
-        
+
         nodes = ChefNodeResource.list_nodes('env', 'role')
-        
+
         expect(nodes.length).to be_eql 1
         expect(nodes[0]).to be_eql 'node'
       end
     end
   end
 
-  describe "#get_single_node" do
+  describe "#find_by_name" do
     context "if node matches the name param" do
       it "should return a single node that matchs the query" do
-        search = mock
+        search = double
         Chef::Search::Query.stub(:new).and_return(search)
         search.stub(:search).with("node", "name:name").and_return([['node']])
 
@@ -31,7 +32,7 @@ describe ChefNodeResource do
         expect(nodes.length).to be_eql 1
         expect(nodes[0]).to be_eql 'node'
       end
-    end    
+    end
   end
 end
 
