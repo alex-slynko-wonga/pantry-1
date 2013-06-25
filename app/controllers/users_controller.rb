9d2afe037fa80d0b6fdeab738b0e5812df0e2371
John Coleman <john-coleman@users.noreply.github.com>
2013-06-25 11:47:06 +0100

    Add format-specific responses to Users controller index and update methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,33 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+ 
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @users }
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.username = params[:username]
+    @user.save
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to(@user,
+                      :notice => "User updated" ) }
+        format.json { render :json => @user,
+                      :status => :updated, :location => @user  }
+      else
+        format.html { redirect_to(@user,
+                      :notice => "User update failed!" ) }
+        format.json { render :json => @user.errors,
+                      :status => :unprocessable_entity  }
+      end
+    end
   end
 end
