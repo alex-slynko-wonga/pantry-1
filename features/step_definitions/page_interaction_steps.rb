bf3f88b8906b3a9c2dfc65b2201c73166790199f
Alex Slynko <slynko@outlook.com>
2013-07-08 16:13:10 +0100

    Revert "Revert "Merge pull request #45 from alex-slynko-wonga/td_940_add_tests_for_removing_members""
    This reverts commit b427226e4df8c9e1a408a536c2e0a8c6dcaf8425.

--- features/step_definitions/page_interaction_steps.rb
+++ features/step_definitions/page_interaction_steps.rb
@@ -5,3 +5,7 @@ end
 When(/^I click (?:on )?"(.*?)"$/) do |text|
   click_on text
 end
+
+When(/^click on remove cross$/) do
+  find('i.icon-remove').click
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,15 @@
 
 FactoryGirl.define do
   factory :user do
+    ignore do
+      team nil
+    end
     username "MyString"
+
+    after(:build) do |user, evaluator|
+      if evaluator.team
+        user.teams << evaluator.team
+      end
+    end
   end
 end
