2ecb459e4c36b20453af69a6ecf7466b5b98a69c
Kieran Manning <kieranmanning@users.noreply.github.com>
2013-07-09 10:39:06 +0100

    TD919 model subtask

--- app/controllers/aws/ec2_instance_controller.rb
+++ app/controllers/aws/ec2_instance_controller.rb
@@ -4,19 +4,6 @@ class Aws::Ec2InstanceController < ApplicationController
   end
 
   def create
-  	Fog.mock!
-  	fog = Fog::Compute.new(provider: 'AWS')
-  	server = fog.servers.create({
-  		flavor_id: "t1.micro",
-  		image_id: "ami-fedfd48a"
-  	})
-  	fog.tags.create(
-  		:resource_id => server.identity,
-  		:key => 'Team',
-  		:value => params["ec2_instance"]["name"]
-  	)
-  	server.reload.tags
-  	puts server
   	redirect_to "/aws/ec2_instance/new"
   end
 end
