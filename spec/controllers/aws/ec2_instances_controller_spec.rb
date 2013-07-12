5b37820c79e7093e75e14a4d9cd198a3107da09a
Kieran Manning <kieranmanning@users.noreply.github.com>
2013-07-12 11:37:24 +0100

    fixing test

--- spec/controllers/aws/ec2_instances_controller_spec.rb
+++ spec/controllers/aws/ec2_instances_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Aws::Ec2InstancesController do
+  before(:each) do 
+    session[:user_id] = FactoryGirl.create(:user).id 
+  end
   let(:user) { FactoryGirl.create(:user) }
   let(:team) { FactoryGirl.create(:team) }
   let(:ec2_instance_params) { 
@@ -21,9 +24,9 @@ describe Aws::Ec2InstancesController do
   describe "POST 'create'" do
     let (:ec2_instance) { Ec2Instance.last }
     it "creates an ec2 instance request record" do
-      #expect{ post :create, ec2_instance_params}.to change(Ec2Instance, :count).by(1)
-      #ec2_instance.name.should == 'InstanceName'
-      #ec2_instance.booted.should == false
+      expect{ post :create, ec2_instance_params}.to change(Ec2Instance, :count).by(1)
+      ec2_instance.name.should == 'InstanceName'
+      ec2_instance.booted.should == false
     end
   end
 end
