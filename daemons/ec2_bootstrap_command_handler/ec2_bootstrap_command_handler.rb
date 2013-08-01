636b146f6b5f64b31f98efc163ecf3978471a381
Alex Slynko <slynko@outlook.com>
2013-08-01 15:14:29 +0100

    TD-1002: Bootstrap daemon execute knife bootstrap

--- daemons/ec2_bootstrap_command_handler/ec2_bootstrap_command_handler.rb
+++ daemons/ec2_bootstrap_command_handler/ec2_bootstrap_command_handler.rb
@@ -1,7 +1,15 @@
-require_relative '../common/ssh_runner'
-require_relative '../common/win_rm_runner'
 require_relative '../common/aws_resource'
 require_relative '../common/publisher'
+require 'chef/knife'
+require 'chef/knife/bootstrap'
+require 'chef/knife/bootstrap_windows_winrm'
+
+Chef::Knife::Bootstrap.load_deps
+Chef::Knife::BootstrapWindowsWinrm.load_deps
+
+require 'chef/application/knife'
+Chef::Knife::Bootstrap.options = Chef::Application::Knife.options.merge(Chef::Knife::Bootstrap.options)
+Chef::Knife::BootstrapWindowsWinrm.options = Chef::Application::Knife.options.merge(Chef::Knife::BootstrapWindowsWinrm.options)
 
 module Daemons
   class EC2BootstrapCommandHandler
@@ -10,29 +18,56 @@ module Daemons
     end
 
     def handle_message(message)
-      return false unless valid_message?(message)
       ec2_instance = AWSResource.new.find_server_by_id message["instance_id"]
       windows = ec2_instance.platform == "windows"
-      machine_address = ec2_instance.private_dns_name || ec2_instance.private_ip_address
 
-      runner = windows ? WinRMRunner.new : SshRunner.new
-      runner.add_host machine_address
-      runner.run_commands "chef-client"
-
+      bootstrap = if windows
+                    Chef::Knife::BootstrapWindowsWinrm.new(message_to_windows_args(message))
+                  else
+                    Chef::Knife::Bootstrap.new(message_to_linux_args(message))
+                  end
+      bootstrap.run
       @publisher.publish(message)
     end
 
-    def valid_message?(message)
-      if message['instance_id'].nil?
-        @publisher.publish_error("Malformed message from SQS missing request_id and/or instance_id: #{message}")
-        return false
-      end
-      unless /^i-[[:xdigit:]]{8}$/.match(message["instance_id"].to_s)
-        @publisher.publish_error("Malformed message from SQS missing request_id and/or instance_id: #{message}")
-        return false
-      end
+    private
+    def message_to_linux_args(message)
+      ["bootstrap",
+       message["ip_address"],
+       "--node-name",
+       "#{message["name"]}.#{message["domain"]}",
+       "--ssh-user",
+       "ubuntu",
+       "--sudo",
+       "--environment",
+       message["chef_environment"],
+       "--identity-file",
+       <%= @config['ssh_key_file'] %>,
+       "--bootstrap-proxy",
+       "http://proxy.example.com:8080",
+       "--run-list",
+       message["run_list"].join("'")]
+    end
 
-      true
+    def message_to_windows_args(message)
+      ["bootstrap",
+       "windows",
+       "winrm",
+       message["ip_address"],
+       "--node-name",
+       "#{message["name"]}.#{message["domain"]}",
+       "--environment",
+       message["chef_environment"],
+       "--bootstrap-proxy",
+       "http://proxy.example.com:8080",
+       "--run-list",
+       message["run_list"].join("'"),
+       "--winrm-password",
+       "LocalAdminPassword",
+       "--winrm-user",
+       "Administrator",
+       "--winrm-transport",
+       "plaintext"]
     end
   end
 end
