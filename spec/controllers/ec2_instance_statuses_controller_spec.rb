050975aeab946bb28a10f0f14a799a2a6810a630
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2013-08-21 10:24:39 +0100

    JenkinsSlaves controller index and ec2 status json
    I added JSON as response for the ec2 status because I would like to call it from the slave views.
    the index view need to display the rows with JavaScript but I want to talk to you before doing that.

--- spec/controllers/ec2_instance_statuses_controller_spec.rb
+++ spec/controllers/ec2_instance_statuses_controller_spec.rb
@@ -2,10 +2,37 @@ require 'spec_helper'
 
 describe Ec2InstanceStatusesController do
   let(:instance) { FactoryGirl.create(:ec2_instance) }
+  
   describe "GET 'show'" do
-    it "returns http success" do
+    it "returns http success with a html request" do
       get 'show', id: instance.id
       response.should be_success
-      end
+    end
+    
+    it "returns a json hash" do
+      expected = { ami: instance.ami,
+                    booted: instance.booted,
+                    bootstrapped: instance.bootstrapped,
+                    chef_environment: instance.chef_environment,
+                    created_at: instance.created_at,
+                    domain: instance.domain,
+                    end_time: instance.end_time,
+                    flavor: instance.flavor,
+                    id: instance.id,
+                    instance_id: instance.instance_id,
+                    joined: instance.joined,
+                    name: instance.name,
+                    platform: instance.platform,
+                    run_list: instance.run_list,
+                    security_group_ids: instance.security_group_ids,
+                    start_time: instance.start_time,
+                    subnet_id: instance.subnet_id,
+                    team_id: instance.team_id,
+                    updated_at: instance.updated_at,
+                    user_id: instance.user_id
+      }.to_json
+      get 'show', id: instance.id, format: :json
+      response.body.should == expected
+    end
   end
 end
--- spec/controllers/jenkins_slaves_controller_spec.rb
+++ spec/controllers/jenkins_slaves_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe JenkinsSlavesController do
+  let(:jenkins_server) {FactoryGirl.create(:jenkins_server)}
+  let(:user) {FactoryGirl.create(:user)}
+  let(:team) {FactoryGirl.create(:team)}
+
+  before(:each) do
+  	session[:user_id] = user.id
+  	user.teams = [team]
+  end
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      slave = FactoryGirl.create(:jenkins_slave, jenkins_server: jenkins_server)
+      jenkins_server.jenkins_slaves << slave
+      get :index, jenkins_server_id: jenkins_server.id, jenkins_slaves: {}
       response.should be_success
+      assigns(:jenkins_server).id.should be jenkins_server.id
+      assigns(:slaves).count.should be 1
     end
   end
 
