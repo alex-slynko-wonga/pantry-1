4191b454bebe95f35035b7975c45a077187d02ef
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2013-08-21 16:18:05 +0100

    Using include in JenkinsSlavesController#index

--- app/controllers/jenkins_slaves_controller.rb
+++ app/controllers/jenkins_slaves_controller.rb
@@ -1,7 +1,7 @@
 class JenkinsSlavesController < ApplicationController
   def index
     @jenkins_server = JenkinsServer.find(params[:jenkins_server_id])
-    @slaves = @jenkins_server.jenkins_slaves
+    @slaves = @jenkins_server.jenkins_slaves.includes(:ec2_instance)
     
     respond_to do |format|
       format.html
