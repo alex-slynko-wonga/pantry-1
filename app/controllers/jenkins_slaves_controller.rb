bb03c456078b2b55641aed1c28fa14945f05b8df
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2013-08-23 11:55:46 +0100

    creation started to work

--- app/controllers/jenkins_slaves_controller.rb
+++ app/controllers/jenkins_slaves_controller.rb
@@ -37,6 +37,30 @@ class JenkinsSlavesController < ApplicationController
     end
   end
   
+  def new
+    @jenkins_slave = JenkinsSlave.new
+    @user_teams = current_user.teams
+  end
+  
+  def create
+    aws_utility = Wonga::Pantry::AWSUtility.new
+    @jenkins_slave = JenkinsSlave.new(jenkins_server: @jenkins_server)
+    attributes = { 
+      user_id: current_user.id, 
+      name: @jenkins_server.team.name,
+      team: @jenkins_server.team,
+      domain: 'example.com',
+    }
+    aws_utility.request_jenkins_instance(attributes, @jenkins_slave)
+    
+    if @jenkins_slave.persisted?
+      redirect_to jenkins_server_jenkins_slave_path(@jenkins_server, @jenkins_slave)
+    else
+      @user_teams = current_user.teams
+      render :new
+    end
+  end
+  
 private
   def load_objects
    @jenkins_server = JenkinsServer.find(params[:jenkins_server_id])
