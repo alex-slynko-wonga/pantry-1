b0d4ea78cbddb594bd4340bdfe6583eccc0319a6
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2013-08-23 15:28:04 +0100

    Completed JenkinsSlave and added specs
    I added controller's specs for both slave and master

--- lib/wonga/pantry/aws_utility.rb
+++ lib/wonga/pantry/aws_utility.rb
@@ -17,11 +17,13 @@ class Wonga::Pantry::AWSUtility
   def request_jenkins_instance(additional_params, jenkins_instance)
     instance_params = jenkins_instance_params.merge(additional_params)
     ec2_instance = Ec2Instance.new(instance_params)
+
     Ec2Instance.transaction do 
       ec2_instance.save!
       jenkins_instance.ec2_instance = ec2_instance
       jenkins_instance.save!
     end
+    
     msg = ec2_instance.boot_message
     if jenkins_instance.persisted?
       @sqs.send_message(msg)
