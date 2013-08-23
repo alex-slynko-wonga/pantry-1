f11be9f95f6a25e4ac2e15da1f0bf75537655bba
Kieran Manning <kieranmanning@users.noreply.github.com>
2013-08-23 10:43:41 +0100

    naming issues

--- spec/lib/wonga/pantry/sqs_sender_spec.rb
+++ spec/lib/wonga/pantry/sqs_sender_spec.rb
@@ -5,8 +5,8 @@ describe Wonga::Pantry::SQSSender do
   let(:message) { ec2.boot_message }
   subject { described_class.new() }  
 
-  describe 'request existing jenkins server' do 
-    it "creates new resource, sends message to AWS and redirects to resource" do
+  describe 'SQSSender' do 
+    it "Sends a message to a queue" do
     client = AWS::SQS.new.client
     resp = client.stub_for(:get_queue_url)
     resp[:queue_url] = "some_url"
