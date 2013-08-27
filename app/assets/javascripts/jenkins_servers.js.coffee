19840d1a8691cb904fcf0fe769e58dc4b8ada7d9
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2013-08-27 11:11:13 +0100

    Changed controller index and completed JS

--- app/assets/javascripts/jenkins_servers.js.coffee
+++ app/assets/javascripts/jenkins_servers.js.coffee
@@ -4,9 +4,7 @@
     unless $scope.team_id is ""
       team_id = $('#single_team_id').attr('data-team_id') # load default team if possible
       team_id = $scope.team_id unless team_id? # otherwise get selected team
-      Instance = $resource("/jenkins_servers?team_id=:team_id", team_id: team_id)
-      $scope.instances = Instance.query()
-      
-  $scope.init = ->
-    $scope.getInstances()
+      if team_id?
+        Instance = $resource("/jenkins_servers?team_id=:team_id", team_id: team_id)
+        $scope.instances = Instance.query()
 ]
--- app/controllers/jenkins_servers_controller.rb
+++ app/controllers/jenkins_servers_controller.rb
@@ -3,9 +3,14 @@ class JenkinsServersController < ApplicationController
   def index
     @user_teams = current_user.teams
     @current_team = @user_teams.find(params[:team_id]) if params[:team_id]
-    @jenkins_servers = JenkinsServer.where("team_id=?", @user_teams.first.id).includes(:ec2_instance) if @user_teams.size == 1
-    @jenkins_servers = JenkinsServer.where(team_id: params[:team_id]).includes(:ec2_instance) if params[:team_id]
-    @jenkins_servers =  JenkinsServer.where("team_id IN(?)", current_user.teams.map(&:id)).includes(:ec2_instance) unless @jenkins_servers
+    @current_team = @user_teams.first if @user_teams && @user_teams.count == 1
+    @jenkins_servers = JenkinsServer.includes(:ec2_instance)
+    
+    if @current_team
+      @jenkins_servers = @jenkins_servers.where(team_id: @current_team.id)
+    else
+      @jenkins_servers = @jenkins_servers.where(team_id: @user_teams.map(&:id))
+    end
   end
 
   def new
