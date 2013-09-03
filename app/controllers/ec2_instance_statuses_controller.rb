877ddcf114df698b778f891eda7b6512f21b4c27
Kieran Manning <kieranmanning@users.noreply.github.com>
2013-09-03 17:12:33 +0100

    further ui touchups

--- app/controllers/ec2_instance_statuses_controller.rb
+++ app/controllers/ec2_instance_statuses_controller.rb
@@ -3,13 +3,15 @@ class Ec2InstanceStatusesController < ApplicationController
   def show
     @ec2_instance = Ec2Instance.find params[:id]
     @statuses = {}
-    [@ec2_instance.joined, @ec2_instance.booted, @ec2_instance.bootstrapped].each{|i| 
-      if i
+    states = [@ec2_instance.booted, @ec2_instance.bootstrapped, @ec2_instance.joined]
+    states.each_with_index{|item, i| 
+      if item
       	@statuses[i] = "awstick.png"
       else
       	@statuses[i] = "spinner.gif"
       end
     }
+    puts @statuses
     respond_to do |format|
       format.html { render layout: false }
       format.json { render json: @ec2_instance }
