57d9309db5a66db612778653e966cd216ca38834
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2013-09-04 15:00:30 +0100

    Load pantry.yml and aws.yml

--- lib/wonga/pantry/sqs_sender.rb
+++ lib/wonga/pantry/sqs_sender.rb
@@ -1,5 +1,5 @@
 class Wonga::Pantry::SQSSender
-  def send_message(instance, queue_name = "pantry_wonga_aws-ec2_boot_command")
+  def send_message(instance, queue_name = CONFIG["aws"]['queue_name'])
     sqs = AWS::SQS.new
     url = sqs.queues.url_for(queue_name)
     sqs.queues[url].send_message(instance.boot_message.to_json)
