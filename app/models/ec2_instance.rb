4d59a6b8f57c45e5459d72555a0314e494e60cf4
Alex Slynko <slynko@outlook.com>
2013-09-05 19:03:20 +0100

    TD-1128: Change domain validation to be configurable

--- app/models/ec2_instance.rb
+++ app/models/ec2_instance.rb
@@ -63,9 +63,9 @@ class Ec2Instance < ActiveRecord::Base
     self.booted ||= false
     self.bootstrapped ||= false
     self.joined ||= false
-    self.domain       ||=  "example.com"
-    self.subnet_id    ||=  "subnet-a8dc0bc0"
-    self.instance_id  ||=  "pending"
+    self.domain       ||= CONFIG['pantry']['domain']
+    self.subnet_id    ||= "subnet-a8dc0bc0"
+    self.instance_id  ||= "pending"
   end
 
   def set_start_time
