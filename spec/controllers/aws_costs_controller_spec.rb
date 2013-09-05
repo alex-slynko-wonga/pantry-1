17a2a56fa191c4aa5755c963ea5f39e4ea1bebd1
Alex Slynko <slynko@outlook.com>
2013-09-05 12:24:24 +0100

    TD-1108: Restrict access to billing page

--- spec/controllers/aws_costs_controller_spec.rb
+++ spec/controllers/aws_costs_controller_spec.rb
@@ -1,21 +1,48 @@
 require 'spec_helper'
 
 describe AwsCostsController do
-  describe 'GET #index' do 
-    it "returns http success with a html request" do
-      get 'index'
-      response.should be_success
-    end  
+  let(:user) { User.new(email: email) }
+  let(:email) { 'some_person@example.com' }
+  before(:each) do
+    @controller.stub(:current_user).and_return(user)
+  end
+
+  describe 'GET #index' do
+    before(:each) { get :index }
+
+    context "when user is jonathan" do
+      let(:email) { 'jonathan.galore@example.com' }
+      specify { expect(response).to be_success }
+    end
+
+    context "when user is not jonathan" do
+      specify { expect(response).to be_redirect }
+    end
   end
 
   describe 'GET #show' do 
-    it "returns an attachment" do 
-      client = AWS::S3::Client.new
-      resp = client.stub_for(:get_object)
-      resp[:data] = "fake value"
+    context "when user is jonathan" do
+      let(:email) { 'jonathan.galore@example.com' }
 
-      @controller.should_receive(:send_data).and_call_original
-      get :show, id: "some.csv"
+      it "returns an attachment" do
+        client = AWS::S3::Client.new
+        resp = client.stub_for(:get_object)
+        resp[:data] = "fake value"
+
+        @controller.should_receive(:send_data).and_call_original
+        get :show, id: "some.csv"
+      end
     end
-  end    
+
+    context "when user is not jonathan" do
+      it "should redirect" do
+        client = AWS::S3::Client.new
+        resp = client.stub_for(:get_object)
+        resp[:data] = "fake value"
+
+        get :show, id: 'some.csv'
+        expect(response).to be_redirect
+      end
+    end
+  end
 end
