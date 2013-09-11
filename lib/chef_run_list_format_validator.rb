1620aee409c39afd089ffacfc2eb8bef1e48c72d
Alex Slynko <slynko@outlook.com>
2013-09-11 19:26:42 +0100

    Fix exception on Jenkins slave

--- lib/chef_run_list_format_validator.rb
+++ lib/chef_run_list_format_validator.rb
@@ -1,7 +1,13 @@
 class ChefRunListFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    if value.split("\r\n").any?{|subvalue| subvalue[/\A(role\[[a-z0-9_]+\]|recipe\[[a-z0-9_]+\]|recipe\[[a-z0-9_]+::[a-z0-9_]*\])\Z/].nil?} 
-      object.errors[attribute] << (options[:message] || "is not formatted properly") 
+    return if value.nil?
+    if value.split("\r\n").any? { |subvalue| invalid_subvalue?(subvalue) }
+      object.errors[attribute] << (options[:message] || "is not formatted properly")
     end
+  end
+
+  private
+  def invalid_subvalue?(subvalue)
+    subvalue[/\A(role\[[a-z0-9_]+\]|recipe\[[a-z0-9_]+\]|recipe\[[a-z0-9_]+::[a-z0-9_]*\])\Z/].nil?
   end
 end
