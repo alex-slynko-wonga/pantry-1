33f1f93013f99831c73d59513cf33c2acdfdf5d9
Alex Slynko <slynko@outlook.com>
2013-09-24 11:27:15 +0100

    Fix api teams controller and tests for it

--- app/controllers/api/teams/chef_environments_controller.rb
+++ app/controllers/api/teams/chef_environments_controller.rb
@@ -1,8 +1,9 @@
 class Api::Teams::ChefEnvironmentsController < ApiController
+  respond_to :json
   def create
     team = Team.find(params[:team_id])
-    team.update_attributes(chef_environment: params[:chef_environment])
+    team.update_attributes(chef_environment: params[:chef_environment][:name])
 
-    respond_with [:api, team]
+    respond_with team
   end
 end
--- spec/controllers/api/teams/chef_environments_controller_spec.rb
+++ spec/controllers/api/teams/chef_environments_controller_spec.rb
@@ -3,32 +3,30 @@ require 'spec_helper'
 describe Api::Teams::ChefEnvironmentsController do
 
   describe "GET 'create'" do
-    
-    let(:team) { mock_model('Team', id: 1, chef_environment: 'env_name') }
-    let(:params) { {team_id: 1, chef_environment: 'env_name', format: 'json'} }
-    
+
+    let(:team) { FactoryGirl.create(:team, chef_environment: nil) }
+    let(:params) { {team_id: team.id, chef_environment: {name: 'env_name'}, format: 'json'} }
+
     context "with valid token" do
       before(:each) do
         request.env['X-Auth-Token'] = CONFIG['pantry']['api_key']
-        Team.stub(:find).with("1").and_return(team)
       end
-      
+
       it "saves chef_environment" do
-        team.should_receive(:update_attributes).with(chef_environment: 'env_name')
-        get 'create', params
+        post 'create', params
+        expect(team.reload.chef_environment).to eq('env_name')
       end
-      
+
       it "returns http success" do
         team.stub(:update_attributes).with(chef_environment: 'env_name')
-        get 'create', params
-        expect(response.status).to eq 200
+        post 'create', params
+        expect(response.status).to eq 201
       end
     end
-    
+
     context "without token" do
       it "returns 404" do
-        team.as_null_object
-        get 'create', params
+        post 'create', params
         expect(response.status).to eq 404
       end
     end
