2b934ac5eccd7e99b80a1812b4546da7e946f2d0
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2013-10-22 15:17:02 +0100

    TD-986_fix2
    Sending slave ID to be used in the event handler
    to call Pantry's API

--- spec/lib/wonga/pantry/jenkins_slave_destroyer_spec.rb
+++ spec/lib/wonga/pantry/jenkins_slave_destroyer_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe Wonga::Pantry::JenkinsSlaveDestroyer do
   let(:ec2_instance) { FactoryGirl.create(:ec2_instance) }
+  let(:jenkins_slave) { FactoryGirl.create(:jenkins_slave, ec2_instance: ec2_instance) }
   let(:user) { FactoryGirl.create(:user) }
-  subject { described_class.new(ec2_instance, 'host_name.domain', 80, user, sqs_sender) }
+  subject { described_class.new(jenkins_slave, 'host_name.domain', 80, user, sqs_sender) }
   let(:sqs_sender) { instance_double('Wonga::Pantry::SQSSender').as_null_object }
 
   context "#destroy" do
     it "saves the user who terminated the slave" do
       ec2_instance.should_receive(:update_attributes).with(terminated_by: user)
-      Wonga::Pantry::JenkinsSlaveDestroyer.new(ec2_instance, 'host_name.domain', 80, user, sqs_sender)
+      Wonga::Pantry::JenkinsSlaveDestroyer.new(jenkins_slave, 'host_name.domain', 80, user, sqs_sender)
     end
     
     it "sends message via SQS" do
