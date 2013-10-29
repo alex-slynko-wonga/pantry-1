77fd9269554b9bbc631d5b45e55bba21a10cbf88
Alex Slynko <slynko@outlook.com>
2013-10-29 11:31:13 +0000

    Fix jenkins server scoping

--- app/models/jenkins_slave.rb
+++ app/models/jenkins_slave.rb
@@ -1,7 +1,6 @@
 class JenkinsSlave < ActiveRecord::Base
   belongs_to :ec2_instance
   belongs_to :jenkins_server
-  has_one :team, through: :jenkins_server
 
   accepts_nested_attributes_for :ec2_instance
 
@@ -9,13 +8,18 @@ class JenkinsSlave < ActiveRecord::Base
 
   before_validation :set_ec2_instance_name
 
-  def set_ec2_instance_name
-    ec2_instance.name = instance_name if ec2_instance
+  def team
+    jenkins_server.try(:team)
   end
 
   def instance_name
     counter = JenkinsSlave.last.try(:id) || 0
-    raise 'There are too many Jenkins Slaves to generate a number lower than 15 characters' if counter > 99999999
+    errors.add(:ec2_instance, 'There are too many Jenkins Slaves to generate a number lower than 15 characters') and return if counter > 99999999
     "agent-#{"%09d" % (counter + 1)}"
+  end
+
+  private
+  def set_ec2_instance_name
+    ec2_instance.name = instance_name if ec2_instance
   end
 end
