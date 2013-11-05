d2f5be5e0b03076f2d2ced3daeca281d54f00997
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2013-11-05 17:03:26 +0000

    Update instance_id key

--- app/controllers/api/costs_controller.rb
+++ app/controllers/api/costs_controller.rb
@@ -1,7 +1,7 @@
 class Api::CostsController < ApiController
   def create
     params[:ec2_total].each do |entry|
-      cost = Ec2InstanceCost.where(ec2_instance_id: entry[:ec2_instance_id], bill_date: params[:bill_date].to_date).first_or_initialize
+      cost = Ec2InstanceCost.where(ec2_instance_id: entry[:instance_id], bill_date: params[:bill_date].to_date).first_or_initialize
       cost.update_attributes(cost: entry[:cost], estimated: entry[:estimated])
     end
 
