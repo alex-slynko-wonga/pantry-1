bbf64f8a4d8263a15b4d0a2f46886e45f52fbbdc
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2014-01-21 18:02:54 +0000

    TD-1569

---
+++ db/migrate/20140121142504_create_environments.rb
@@ -0,0 +1,29 @@
+class CreateEnvironments < ActiveRecord::Migration
+  def up
+    create_table :environments do |t|
+      t.string :name
+      t.string :description
+      t.string :chef_environment
+      t.integer :team_id
+
+      t.timestamps
+    end
+
+    Ec2Instance.pluck(:chef_environment, :team_id).uniq.each do |environment, team_id|
+      Environment.create(name: environment.gsub('-', ' ').humanize, chef_environment: environment, team_id: team_id)
+    end
+
+    add_column :ec2_instances, :environment_id, :integer
+    Ec2Instance.reset_column_information
+
+    Ec2Instance.all.each do |instance|
+      env = Environment.where(chef_environment: instance.chef_environment, team_id: instance.team_id).first
+      instance.update_attributes(environment: env)
+    end
+  end
+
+  def down
+    remove_column :ec2_instances, :environment_id, :integer
+    drop_table :environments
+  end
+end
---
+++ spec/factories/environments.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :environment do
+    name "MyText"
+    description "MyText"
+    env_type "MyText"
+  end
+end
