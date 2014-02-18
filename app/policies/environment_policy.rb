39981e6d406cb0a01ca602f589dd74442611091a
Alex Slynko <slynko@outlook.com>
2014-02-18 13:47:13 +0000

    TD-1857 Add new environment type - CUSTOM

--- app/policies/environment_policy.rb
+++ app/policies/environment_policy.rb
@@ -1,4 +1,11 @@
 class EnvironmentPolicy < ApplicationPolicy
+  def permitted_types
+    types = Environment::TYPES.dup
+    types.delete('CI') if @record.team.ci_environment
+    types.delete('CUSTOM') unless god_mode?
+    types
+  end
+
   def create?
     god_mode? || team_member?
   end
