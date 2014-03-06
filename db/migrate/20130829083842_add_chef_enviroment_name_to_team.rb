eed8c095619c454b539161bac9ff173fc3d02738
Alex Slynko <slynko@outlook.com>
2014-03-06 11:11:03 +0000

    Fix migrations

--- db/migrate/20130829083842_add_chef_enviroment_name_to_team.rb
+++ db/migrate/20130829083842_add_chef_enviroment_name_to_team.rb
@@ -2,7 +2,7 @@ class AddChefEnviromentNameToTeam < ActiveRecord::Migration
   def change
     add_column :teams, :chef_environment, :string
     Team.reset_column_information
-    Team.all.each do |team|
+    Team.unscoped.all.each do |team|
       team.update_attribute(:chef_environment, Wonga::Pantry::ChefEnvironmentBuilder.new(team).chef_environment)
     end
   end
