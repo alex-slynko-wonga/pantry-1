7b3113fc2d1e91e34fecc4b694d73d5b75ef70b2
Riccardo Tacconi <rtacconi@users.noreply.github.com>
2014-05-01 14:31:45 +0100

    TD-2177

--- spec/policies/ec2_adapter_policy_spec.rb
+++ spec/policies/ec2_adapter_policy_spec.rb
@@ -6,10 +6,12 @@ describe Ec2AdapterPolicy do
   context "when user is admin" do
     let(:user) { FactoryGirl.build(:superadmin) }
     it { should permit(:show_all_security_groups?) }
+    it { should permit(:show_all_subnets?) }
   end
 
   context "when user is not an admin" do
     let(:user) { FactoryGirl.build(:user) }
     it { should_not permit(:show_all_security_groups?) }
+    it { should_not permit(:show_all_subnets?) }
   end
 end
