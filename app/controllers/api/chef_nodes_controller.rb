3556707e5dfa53a40fa6c3174e6b7741e7292e36
John Coleman <john-coleman@users.noreply.github.com>
2014-06-03 14:03:16 +0100

    TD-2281 Update API Controllers not to require user_id parameter

--- app/controllers/api/chef_nodes_controller.rb
+++ app/controllers/api/chef_nodes_controller.rb
@@ -1,6 +1,6 @@
 class Api::ChefNodesController < ApiController
   def destroy
-    user = User.find(params[:user_id])
+    user = User.where(id: params[:user_id]).first || nil
     ec2_instance = Ec2Instance.find(params[:id])
     Wonga::Pantry::Ec2InstanceState.new(ec2_instance, user, {'event' => :terminated, 'bootstrapped' => false}).change_state
     respond_with {}
