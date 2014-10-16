c8d7b1c946541880c91827d607a6d11ff1be414e
Alex Slynko <slynko@outlook.com>
2014-10-16 18:37:08 +0100

    Clean Ec2Adapter
    * Rubocopify
    * Made ec2 method private
    
    Change-Id: I0e94385f27806229bde7eeb0d5a5f2c4fb60a60f

--- lib/wonga/pantry/ec2_adapter.rb
+++ lib/wonga/pantry/ec2_adapter.rb
@@ -15,7 +15,9 @@ class Wonga::Pantry::Ec2Adapter
 
   def get_ami_attributes(ami)
     return if ami.blank?
-    return unless image = ec2.client.describe_images(image_ids: [ami])[:images_set][0]
+    image = ec2.client.describe_images(image_ids: [ami])[:images_set][0]
+    return unless image
+
     image[:platform] ||= 'linux'
     image
   rescue AWS::EC2::Errors::InvalidAMIID::NotFound, AWS::EC2::Errors::InvalidAMIID::Malformed
@@ -31,32 +33,31 @@ class Wonga::Pantry::Ec2Adapter
 
   def security_groups
     groups = ec2.client.describe_security_groups(
-      :filters => [ { :name =>"vpc-id",
-                      :values => [CONFIG['aws']['vpc_id']] } ]
+      filters: [{ name: 'vpc-id',
+                  values: [CONFIG['aws']['vpc_id']] }]
     )[:security_group_info].map do |group|
-      [ group[:group_name], group[:group_id] ]
+      [group[:group_name], group[:group_id]]
     end.sort
 
-    groups.select! {|(name, _)| name[SECURITY_GROUPS_REGEX] } unless ec2_adapter_policy.show_all_security_groups?
-    groups.delete_if {|(_, id)| id == CONFIG['aws']['security_group_linux'] || id == CONFIG['aws']['security_group_windows']}
+    groups.select! { |(name, _)| name[SECURITY_GROUPS_REGEX] } unless ec2_adapter_policy.show_all_security_groups?
+    groups.delete_if { |(_, id)| id == CONFIG['aws']['security_group_linux'] || id == CONFIG['aws']['security_group_windows'] }
   end
 
   def subnets
     if ec2_adapter_policy.show_all_subnets?
       ec2.subnets
     else
-      ec2.subnets.filter("tag:Network", "Private*")
-    end.map{|s| [s.tags["Name"] || s.id, s.id]}
+      ec2.subnets.filter('tag:Network', 'Private*')
+    end.map { |s| [s.tags['Name'] || s.id, s.id] }
   end
+
+  private
 
   def ec2
     @ec2 ||= AWS::EC2.new
   end
 
-  private
-
   def ec2_adapter_policy
     Ec2AdapterPolicy.new(@user)
   end
 end
-
