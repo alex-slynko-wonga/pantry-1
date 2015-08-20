a429cbb3c8aeedd793fa449f2f3cbf7dd6edb534
John Coleman <john-coleman@users.noreply.github.com>
2015-08-20 16:02:33 +0100

    RM-2061_Prepare_for_opensourcing
    * Removing more details, snapshot ids to make a clean HEAD for bfg
    
    Change-Id: I82ffa67a9176ebc2db04aedd19507d2eb7d50bcd

--- spec/lib/wonga/pantry/jenkins_slave_destroyer_spec.rb
+++ spec/lib/wonga/pantry/jenkins_slave_destroyer_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Wonga::Pantry::JenkinsSlaveDestroyer do
   let(:ec2_instance) { jenkins_slave.ec2_instance }
   let(:user) { FactoryGirl.create(:user, team: team) }
   let(:team) { FactoryGirl.create(:team) }
-  subject { described_class.new(jenkins_slave, 'host_name.domain', 80, user, sns_publisher) }
+  subject { described_class.new(jenkins_slave, 'host.example.com', 80, user, sns_publisher) }
   let(:sns_publisher) { instance_double('Wonga::Pantry::SNSPublisher').as_null_object }
   let(:message) do
     {
-      'server_fqdn' => 'host_name.domain',
+      'server_fqdn' => 'host.example.com',
       'server_port' => 80,
       'hostname' => ec2_instance.name,
       'domain' => ec2_instance.domain,
